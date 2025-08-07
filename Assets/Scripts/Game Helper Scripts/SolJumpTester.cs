6ec7c0c822ecc62356e1d9baf08f5a3010984a8b
WeUseCrrtzzz <jklmonth@gmail.com>
2025-08-07 17:36:26 +0100

    Updated jumping

--- Assets/Scripts/Game Helper Scripts/SolJumpTester.cs
+++ Assets/Scripts/Game Helper Scripts/SolJumpTester.cs
@@ -7,12 +7,20 @@ public class SolJumpTester : MonoBehaviour
 
     void Start()
     {
-        Button jumpA = GameObject.Find("JumpA").GetComponent<Button>();
-        Button jumpB = GameObject.Find("JumpB").GetComponent<Button>();
-        Button jumpC = GameObject.Find("JumpC").GetComponent<Button>();
+        //Button jumpA = GameObject.Find("JumpA").GetComponent<Button>();
+        //Button jumpB = GameObject.Find("JumpB").GetComponent<Button>();
+        //Button jumpC = GameObject.Find("JumpC").GetComponent<Button>();
 
-        jumpA.onClick.AddListener(() => jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.A));
-        jumpB.onClick.AddListener(() => jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.B));
-        jumpC.onClick.AddListener(() => jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.C));
+        //jumpA.onClick.AddListener(() => jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.A));
+        //jumpB.onClick.AddListener(() => jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.B));
+        //jumpC.onClick.AddListener(() => jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.C));
+    }
+
+    public void Jump() 
+    {
+        if (Resources.fuel >= 50 && Resources.fuel < 75) jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.A);
+        if (Resources.fuel >= 75 && Resources.fuel < 100) jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.B);
+        if (Resources.fuel >= 100) jumpManager.JumpToNewSol(SolJumpManager.JumpLevel.C);
+        Resources.fuel = 0;
     }
 }
